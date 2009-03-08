20d469dbebb93d02ea1f9970a8fc74340f9d4515
Dmitrij <kostindima@yandex.ru>
2009-03-08 23:17:05 +0000

    first commit

---
+++ chessplugin.cpp
@@ -0,0 +1,133 @@
+#include <qutim/plugininterface.h>
+#include "chessplugin.h"
+#include "QMessageBox"
+
+extern QColor	cw, cb;
+
+bool chessPlugin::init ( PluginSystemInterface *plugin_system )
+{
+    qRegisterMetaType<TreeModelItem> ( "TreeModelItem" );
+
+    PluginInterface::init ( plugin_system );
+    m_plugin_icon = new QIcon ( ":/icons/plugin.png" );
+    m_plugin_system = plugin_system;
+    TreeModelItem contact;
+
+    m_event_receive = m_plugin_system->registerEventHandler("Core/ChatWindow/ReceiveLevel2", this);
+
+    QAction* menuaction = new QAction(QIcon(":/icons/chessplugin.png"), tr("Play chess"), this);
+    connect(menuaction, SIGNAL(triggered()), this,  SLOT(actionStart()));
+    m_item_menu = plugin_system->registerEventHandler("Core/ContactList/ContactContext", this);
+    m_plugin_system->registerContactMenuAction(menuaction, this);
+
+    return true;
+}
+
+void chessPlugin::processEvent ( Event &event )
+{
+    if ( event.id == m_item_menu ) {
+	eventitem = event.at<TreeModelItem>(0);
+    }
+
+    if ( event.id == m_event_receive) {
+	QString &message = event.at<QString>(1);
+	if (!message.startsWith("chess"))
+		    return;
+
+	QString command = QString(message);
+	command.remove(0,6);
+	qDebug() << (qPrintable(QString("chess command string %1").arg(command)));
+	TreeModelItem from = event.at<TreeModelItem>(0);
+	QString fromJid = from.m_item_name;
+	bool &b = event.at<bool>(2);
+	b = true;
+
+	if (command == QString("start"))
+	{
+		int ret = QMessageBox::question(0, tr("Qutim chess plugin"),
+				fromJid + tr(" invites you to play chess.\n"
+				   "Accept?"),
+				QMessageBox::Yes | QMessageBox::No);
+		if ( ret == QMessageBox::Yes )
+		{
+		    m_plugin_system->sendCustomMessage(from, "chess starting", true);
+		    startGame(fromJid, false, from);
+		} else {
+		    m_plugin_system->sendCustomMessage(from, "chess cancel", true);
+		}
+	}
+	else if (command == QString("starting"))
+	{
+		m_plugin_system->sendCustomMessage(from, "starting chess, I go first :)", true);
+		startGame(fromJid, true, from);
+	}
+	else if (command == QString("cancel"))
+	{
+	    QMessageBox::information(0, tr("Qutim chess plugin"),
+				fromJid + tr(" don't accept your invite to play chess."),
+				QMessageBox::Yes);
+	}
+	else if (!game_)
+	{
+		return;
+	}
+	else if (command.startsWith("command"))
+	{
+		command.remove(0,8);
+
+		qDebug() << (qPrintable(QString("chess command %1").arg(command)));
+		receiveData(command);
+	}
+    }
+}
+
+void chessPlugin::actionStart()
+{
+     m_plugin_system->sendCustomMessage(eventitem, "chess start", true);
+}
+
+void chessPlugin::startGame(const QString& jid, bool meFirst, const TreeModelItem item)
+{
+	playingWith_=jid;
+	m_with = item;
+	cw = QColor(0x8F, 0xDF, 0xF0);
+	cb = QColor(0x70, 0x9F, 0xDF);
+	if (meFirst)
+	{
+		GameBoard::GameType type=GameBoard::WHITE;
+		game_ = new GameBoard(type, jid, NULL);
+	} else {
+		game_ = new GameBoard(1);
+	}
+
+	connect(game_, SIGNAL(sendData(const QString&)), this, SLOT(sendData(const QString &)));
+	game_->show();
+ }
+
+void chessPlugin::stopGame()
+{
+	delete game_;
+	game_=NULL;
+}
+
+void chessPlugin::sendData(const QString& data)
+{
+	qDebug() << (qPrintable(QString("sendingData turn: %1").arg(data)));
+	if (!game_)
+		return;
+	QString reply;
+	reply=QString("chess command %1").arg(data);
+	qDebug() << (qPrintable(QString("sendingData: %1").arg(reply)));
+	m_plugin_system->sendCustomMessage(m_with, reply, true);
+}
+
+void chessPlugin::receiveData(const QString& data)
+{
+	qDebug() << (qPrintable(QString("received data: %1").arg(data)));
+	if (!game_)
+		return;
+	//game->theirMove(space);
+	game_->receiveData(data);
+}
+
+Q_EXPORT_PLUGIN2 ( chessplugin,chessPlugin );
---
+++ chessplugin.h
@@ -0,0 +1,51 @@
+#ifndef chessPLUGIN_H
+#define chessPLUGIN_H
+
+#include "qutim/plugininterface.h"
+#include "gameboard.h"
+#include <QAction>
+
+using namespace qutim_sdk_0_2;
+
+class chessPlugin : public QObject, SimplePluginInterface, EventHandler
+{
+    Q_OBJECT
+    Q_INTERFACES ( qutim_sdk_0_2::PluginInterface )
+public:
+    virtual bool init ( PluginSystemInterface *plugin_system );
+    virtual void release(){ }
+
+    virtual void processEvent ( PluginEvent &event ) {}
+    virtual void processEvent ( Event &event );
+    virtual QWidget *settingsWidget(){return new QWidget();}
+    virtual void setProfileName ( const QString &profile_name ) { m_profile_name=profile_name; }
+    virtual QString name(){ return "Chess plugin"; }
+    virtual QString description(){ return "Plugin to play chess over different protocols"; }
+    virtual QString type(){ return "game"; }
+    virtual QIcon *icon(){ return m_plugin_icon; }
+    virtual void removeSettingsWidget(){}
+    virtual void saveSettings(){}
+private:
+    QIcon *m_plugin_icon;
+    QString m_profile_name;
+    QString m_account_name;
+    quint16 m_event_receive;
+    quint16 m_item_menu;
+
+    GameBoard* game_;
+    QString playingWith_;
+
+    void startGame(const QString& jid, bool meFirst, const TreeModelItem item);
+    void stopGame();
+
+    TreeModelItem m_with;
+    TreeModelItem eventitem;
+
+private slots:
+    void sendData(const QString& data);
+    void receiveData(const QString& data);
+    void actionStart();
+};
+
+#endif
+
